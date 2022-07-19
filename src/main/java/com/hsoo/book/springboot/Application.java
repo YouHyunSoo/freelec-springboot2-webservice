92c41d07fa88e17691aac5b97d3e7e5a5dd5ff4f
YouHyunSoo <xowkd31226@gmail.com>
2022-07-19 17:49:30 +0900

    Application , HelloController, HelloControllerTest 추가

---
+++ src/main/java/com/hsoo/book/springboot/Application.java
@@ -0,0 +1,12 @@
+package com.hsoo.book.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args); // 내장 WAS 실행, 스프링부트는 내장 WAS 사용을 권장
+                                                        // 이유는 언제어디서나 같은 환경에서 배포 가능하기때문에
+    }
+}

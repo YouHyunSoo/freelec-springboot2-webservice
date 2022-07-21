80fefbe7a34fd69971920623b2f72e26537ca569
YouHyunSoo <xowkd31226@gmail.com>
2022-07-21 17:07:25 +0900

    22/07/21 등록,수정,조회 API 만들기 진행

--- src/test/java/com/hsoo/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/hsoo/book/springboot/web/HelloControllerTest.java
@@ -7,9 +7,10 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(controllers = HelloController.class)
@@ -25,5 +26,18 @@ public class HelloControllerTest {
         mvc.perform(get("/hello"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(hello));
+    }
+
+    @Test
+    public void helloDto가_리턴된다() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(get("/hello/dto")
+                    .param("name", name)    //param : API 테스트할 때 사용될 요청 파라미터를 설정, 값은 String만 허용
+                    .param("amount", String.valueOf(amount)))   // 숫자/날짜 등의 데이터도 등록할 때에는 문자열로 변경해야함
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.name", is(name)))    // jsonPath : JSON 응답값을 필드별로 검증할 수 있는 메소드
+                    .andExpect(jsonPath("$.amount", is(amount)));   //$ 기준으로 필드명을 명시..
     }
 }
---
+++ src/test/java/com/hsoo/book/springboot/web/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,50 @@
+package com.hsoo.book.springboot.web.domain.posts;
+
+import com.hsoo.book.springboot.domain.posts.Posts;
+import com.hsoo.book.springboot.domain.posts.PostsRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @AfterEach  // junit에서 단위 테스트가 끝날 때마다 수행되는 메소드를 지정
+    public void cleanup(){
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기(){
+        //given
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        // postsRepository.save 테이블 posts에 insert/update 쿼리를 실행, id 값이 있으면 update 없으면 insert
+        postsRepository.save(Posts.builder()
+                        .title(title)
+                        .content(content)
+                        .author("hsoo@gmail.com")
+                        .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+        // postsRepository.findAll(); 테이블 posts에 있는 모든 데이터를 조회해오는 메소드
+
+        //then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+}
